4cc15910d0552004872b52b180369e49da83a49a
Simone Mele <simonemele999@gmail.com>
2020-03-29 17:52:41 +0200

    Bug fix

--- lib/Utility/AcceptJob.dart
+++ lib/Utility/AcceptJob.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 
 class AcceptJob {
-  static accept(listID,deliveryManID) async {
+  static accept(listID, deliveryManID) async {
     var dio = Dio();
-    String Url = "http://79.37.45.118:8080/deliverymans/${deliveryManID}/shippingLists/${listID}/accept";
-    Response response = await dio.get(Url);
+    String Url =
+        "http://79.37.45.118:8080/deliverymans/${deliveryManID}/shippingLists/${listID}/accept";
+    Response response = await dio.put(Url);
   }
 }
